8e0fdd257933b5d75cf5b54f4cd97f0409952e0a
Gayathri1415 <36990581+Gayathri1415@users.noreply.github.com>
2018-03-09 18:02:59 +0530

    Create largenum.py

---
+++ largenum.py
@@ -0,0 +1,11 @@
+a=10
+b=20
+c=15
+if (a >= b) and (a >= c):
+   largest = a
+elif (b >= a) and (b >= c):
+   largest = b
+else:
+   largest = c
+
+print("The largest number between",a,",",b,"and",c,"is",largest)
