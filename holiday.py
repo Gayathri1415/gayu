fad3a1bcf533411b0df5f27e7df65f5428559bf6
Gayathri1415 <36990581+Gayathri1415@users.noreply.github.com>
2018-03-12 17:59:56 +0530

    Create holiday.py

---
+++ holiday.py
@@ -0,0 +1,12 @@
+print("DAYS:sunday,monday,tuesday,wednesday,thursday,friday,saturday,")
+day = input("enter the day:") 
+
+
+if day in ('saturday','sunday'):
+	
+  print("is a holiday")
+
+
+else:
+	
+  print("is a not holiday") 
