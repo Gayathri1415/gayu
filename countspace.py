c3a7b46792f58b2aa7050d081727ff1502eba932
Gayathri1415 <36990581+Gayathri1415@users.noreply.github.com>
2018-05-24 12:32:02 +0530

    Update countspace.py

--- countspace.py
+++ countspace.py
@@ -1,11 +1,7 @@
-fname = input("Enter file name: ")
-k = 0
-with open(fname, 'r') as f:
-    for line in f:
-        words = line.split()
-        for i in words:
-            for letter in i:
-                if(letter.isspace):
-                    k=k+1
-print("Occurrences of blank spaces:")
-print(k)
+s=input('enter a string:')
+space=0
+for i in s:
+  if (i.isspace()):
+    space=space+1
+print (space)
+
