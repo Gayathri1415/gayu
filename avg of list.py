1bae4068ca8db01dd2879b155f8e98c41940a6e8
Gayathri1415 <36990581+Gayathri1415@users.noreply.github.com>
2018-05-24 10:07:55 +0530

    Create avg of list.py

---
+++ avg of list.py
@@ -0,0 +1,13 @@
+a=int(input('no.of elements:'))
+sum=0
+l=[]
+for i in range(1,a+1):
+  n=int(input('enter a number'))
+  l.append(n)
+print (l)
+for x in l:
+  sum=sum+x
+  b=(sum/a)
+print(b)
+
+  
