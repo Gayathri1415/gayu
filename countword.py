8e6cede0c989f53074298629f49a8d498676d935
Gayathri1415 <36990581+Gayathri1415@users.noreply.github.com>
2018-05-24 12:16:11 +0530

    Update countword.py

--- countword.py
+++ countword.py
@@ -1,8 +1,6 @@
-fname = input("Enter file name: ")
-num_words = 0
-with open(fname, 'r') as f:
-    for line in f:
-        words = line.split()
-        num_words += len(words)
-print("Number of words:")
-print(num_words)
+string=input("enter a string")
+word=1
+for i in string:
+    if(i==" "):
+        word=word+1
+print (word)
